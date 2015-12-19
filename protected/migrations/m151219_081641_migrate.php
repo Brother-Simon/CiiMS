a7eaccb3d0028db7708cacec88eb123c2b88303c
simon <mahuan1768@126.com>
2015-12-19 00:25:47 -0800

    好使  Changes to be committed: 	new file:   protected/migrations/m151219_081641_migrate.php

---
+++ protected/migrations/m151219_081641_migrate.php
@@ -0,0 +1,25 @@
+<?php
+
+class m151219_081641_migrate extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m151219_081641_migrate does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
